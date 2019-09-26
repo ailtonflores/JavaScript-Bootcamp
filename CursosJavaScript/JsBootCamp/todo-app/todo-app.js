3e30bd5d3643e0e34305fde273234b728aade12a
ailton flores <ailtonflores20@gmail.com>
2019-09-26 17:21:47 -0300

    Classe 7-8 Using third-party library- UUIDv4 added

--- CursosJavaScript/JsBootCamp/todo-app/todo-app.js
+++ CursosJavaScript/JsBootCamp/todo-app/todo-app.js
@@ -1,3 +1,4 @@
+
 const todos = getsavedtodos()
 
 const filters = {
@@ -19,6 +20,7 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.preventDefault()
     todos.push({
+        id: uuidv4(),
         text: e.target.elements.text.value,
         completed: false
     })
